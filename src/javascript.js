3e9ef0cf109df923e257052bf46e7116efb91e0d
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-01-08 22:42:09 +0000

    calling the coordinates of API to provide the daily forecast

--- src/javascript.js
+++ src/javascript.js
@@ -40,7 +40,8 @@ function formatDate(date) {
   return `${day}, ${todayDate} ${month} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
@@ -62,6 +63,13 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ba961258607b8a685407ad6954262a1c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let dateElement = document.querySelector("#currentDate");
@@ -88,6 +96,8 @@ function displayCurrentWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 
   // document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
   //document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
@@ -148,4 +158,3 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
 
 onLoad("London");
-displayForecast();
